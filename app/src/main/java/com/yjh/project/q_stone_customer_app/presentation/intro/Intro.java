5b230ccb91761dcd695921befb0f3a01573e0051
Yun Jung Hyeon <yjh54240@naver.com>
2018-08-07 21:50:41 +0900

    commit

--- app/src/main/java/com/yjh/project/q_stone_customer_app/presentation/intro/Intro.java
+++ app/src/main/java/com/yjh/project/q_stone_customer_app/presentation/intro/Intro.java
@@ -3,26 +3,47 @@ package com.yjh.project.q_stone_customer_app.presentation.intro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.yjh.project.q_stone_customer_app.R;
 
 public class Intro extends AppCompatActivity {
 
+    TextView loginTextView;
+    Button signUpButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        loginTextView=findViewById(R.id.login_text);
+        signUpButton=findViewById(R.id.SignUp);
+
+        init();
+
     }
 
-    public void LoginClick() {
-        Intent intent = new Intent(Intro.this, Login.class);
-        startActivity(intent);
-        finish();
-    }
 
-    public void SignUpClick() {
-        Intent intent = new Intent(Intro.this, SignUp.class);
-        startActivity(intent);
-        finish();
+    private void init(){
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intro.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intro.this, SignUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/yjh/project/q_stone_customer_app/presentation/intro/Login.java
+++ app/src/main/java/com/yjh/project/q_stone_customer_app/presentation/intro/Login.java
@@ -1,15 +1,30 @@
 package com.yjh.project.q_stone_customer_app.presentation.intro;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.yjh.project.q_stone_customer_app.R;
+import com.yjh.project.q_stone_customer_app.presentation.main.MainActivity;
 
 public class Login extends AppCompatActivity {
 
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton=findViewById(R.id.emailLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
